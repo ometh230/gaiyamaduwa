4df94b87884ae79589d4a6a763d485135f905326
ometh123 <omethgeesan230@gmail.com>
2026-03-22 10:38:36 +0530

    Update database connection string in config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,4 +1,4 @@
-const DATABASE = "mongodb+srv://gayanmadura998_db_user:CL6kaPlHyimazK5G@madawage.aqcei7c.mongodb.net/?appName=madawage" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
+const DATABASE = "mongodb+srv://gayanayya324_db_user:ZwrGF7lRl49pwu8v@cluster0.winjy3a.mongodb.net/?appName=Cluster0" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
 const BOT_NUMBER = "94764802314" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
 const SESSION_ID = "PRABATH-MD~8y58MSEnxmoFTJG" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
